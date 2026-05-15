b5a46daf30fa38c7d84b2165f074490a60c4d3f8
supaangaaa . <rajdhungel63@gmail.com>
2026-05-15 16:54:26 +0545

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="p-8 text-center bg-green">
+      <h1 className="text-5xl font-bold text-blue-600">SewaMandala Landing Page</h1>
+    </div>
+  )
+}
+export default App
